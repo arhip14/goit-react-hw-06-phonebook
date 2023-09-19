81368df3b223739ba3adb563038222b3a4b9f610
arhip14 <kruvdyk1503@gmail.com>
2023-09-19 21:01:17 +0300

    edit ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,32 @@
-import React from "react";
-import { ContactListContainer } from "./ContactList.styles";
+import React from 'react';
+import { UseSelector, useDispatch, useSelector } from 'react-redux';
+import { deleteContact } from 'components/Redux/actions/contactsActions';
+import { ContactListContainer } from './ContactList.styles';
 
-const ContactList = ({ contacts, onDeleteContact }) => {
+const ContactList = () => {
+  const contact = useSelector((state) => state.contacts.contacts);
+  const filter = useSelector((state) => state.contacts.filter)
+  const dispatch = useDispatch();
+
+  const filteredContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(filter.toLowerCase()));
+
+  const handleDelete = (id) => {
+    dispatch(deleteContact(id));
+  };
+
   return (
     <ContactListContainer>
       <ul>
-        {contacts.map((contact) => (
+        {filteredContacts.map((contact) => (
           <li key={contact.id}>
             {contact.name}: {contact.number}
-            <button className="delete-button" onClick={() => onDeleteContact(contact.id)}>
-              Delete
-            </button>
+            <button onClick={() => handleDelete(contact.id)}>Delete</button>
           </li>
         ))}
       </ul>
     </ContactListContainer>
   );
 };
-
 
 export default ContactList;
